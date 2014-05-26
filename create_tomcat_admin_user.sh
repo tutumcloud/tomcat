12f0f0366fe039ff121b3ce4a1fa9c64c2b363d6
tifayuki <tifayuki@gmail.com>
2014-05-26 17:05:15 -0400

    correct admin roles

--- create_tomcat_admin_user.sh
+++ create_tomcat_admin_user.sh
@@ -12,9 +12,11 @@ _word=$( [ ${TOMCAT_PASS} ] && echo "preset" || echo "random" )
 echo "=> Creating and admin user with a ${_word} password in Tomcat"
 sed -i -r 's/<\/tomcat-users>//' ${CATALINA_HOME}/conf/tomcat-users.xml
 echo '<role rolename="manager-gui"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
-echo '<role rolename="manager-scripti"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
-echo '<role rolename="manager-jmx"/>\n<role rolename="admin-gui"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
-echo "<role rolename=\"admin-script\"/>\n<user username=\"admin\" password=\"${PASS}\" roles=\"manager-gui,manager-script,manager-jmx,admin-gui, admin-script\"/>" >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo '<role rolename="manager-script"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo '<role rolename="manager-jmx"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo '<role rolename="admin-gui"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo '<role rolename="admin-script"/>' >> ${CATALINA_HOME}/conf/tomcat-users.xml
+echo "<user username=\"admin\" password=\"${PASS}\" roles=\"manager-gui,manager-script,manager-jmx,admin-gui, admin-script\"/>" >> ${CATALINA_HOME}/conf/tomcat-users.xml
 echo '</tomcat-users>' >> ${CATALINA_HOME}/conf/tomcat-users.xml 
 echo "=> Done!"
 touch /.tomcat_admin_created
